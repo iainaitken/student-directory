6e16b53e25a2117aed492cd874ff67197e48427c
sleepybadger80 <iain_aitken@yahoo.co.uk>
2021-01-18 18:16:12 +0000

    update directory_exercises.rb (exercise 5)

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,16 +1,16 @@
 
 students = [ 
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
+  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Cooking", country_of_birth: "USA", height: "178cm"},
+  {name: "Darth Vader", cohort: :november, hobbies: "The Force", country_of_birth: "Space", height: "187cm"},
+  {name: "Nurse Ratched", cohort: :november, hobbies: "Nursing", country_of_birth: "USA", height: "159cm"},
+  {name: "Michael Corleone", cohort: :november, hobbies: "Shooting", country_of_birth: "Italy", height: "174cm"},
+  {name: "Alex DeLarge", cohort: :november, hobbies: "Tiddlywinks", country_of_birth: "USA", height: "169cm"},
+  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "Wearing a pointy hat", country_of_birth: "Oz", height: "158cm"},
+  {name: "Terminator", cohort: :november, hobbies: "Search and destroy", country_of_birth: "USA", height: "190cm"},
+  {name: "Freddy Krueger", cohort: :november, hobbies: "Arts and crafts", country_of_birth: "USA", height: "180cm"},
+  {name: "The Joker", cohort: :november, hobbies: "Comedy", country_of_birth: "USA", height: "172cm"},
+  {name: "Joffrey Baratheon", cohort: :november, hobbies: "Despotism", country_of_birth: "Westeros", height: "167cm"},
+  {name: "Norman Bates", cohort: :november, hobbies: "Hospitality", country_of_birth: "USA", height: "181cm"}
 ]
 
 =begin
