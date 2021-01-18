a4901bf029ae2bb133dad066187950a40bf8e9bc
sleepybadger80 <iain_aitken@yahoo.co.uk>
2021-01-18 19:44:31 +0000

    update directory_exercises.rb (exercises 8 and 9)

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -1,19 +1,19 @@
-=begin
+
 students = [ 
-  {name: "Dr. Hannibal Lecter", cohort: :november, hobbies: "Cooking", country_of_birth: "USA", height: "178cm"},
+  {name: "Dr. Hannibal Lecter", cohort: :december, hobbies: "Cooking", country_of_birth: "USA", height: "178cm"},
   {name: "Darth Vader", cohort: :november, hobbies: "The Force", country_of_birth: "Space", height: "187cm"},
   {name: "Nurse Ratched", cohort: :november, hobbies: "Nursing", country_of_birth: "USA", height: "159cm"},
-  {name: "Michael Corleone", cohort: :november, hobbies: "Shooting", country_of_birth: "Italy", height: "174cm"},
+  {name: "Michael Corleone", cohort: :october, hobbies: "Shooting", country_of_birth: "Italy", height: "174cm"},
   {name: "Alex DeLarge", cohort: :november, hobbies: "Tiddlywinks", country_of_birth: "USA", height: "169cm"},
-  {name: "The Wicked Witch of the West", cohort: :november, hobbies: "Wearing a pointy hat", country_of_birth: "Oz", height: "158cm"},
-  {name: "Terminator", cohort: :november, hobbies: "Search and destroy", country_of_birth: "USA", height: "190cm"},
+  {name: "The Wicked Witch of the West", cohort: :october, hobbies: "Wearing a pointy hat", country_of_birth: "Oz", height: "158cm"},
+  {name: "Terminator", cohort: :december, hobbies: "Search and destroy", country_of_birth: "USA", height: "190cm"},
   {name: "Freddy Krueger", cohort: :november, hobbies: "Arts and crafts", country_of_birth: "USA", height: "180cm"},
   {name: "The Joker", cohort: :november, hobbies: "Comedy", country_of_birth: "USA", height: "172cm"},
-  {name: "Joffrey Baratheon", cohort: :november, hobbies: "Despotism", country_of_birth: "Westeros", height: "167cm"},
+  {name: "Joffrey Baratheon", cohort: :december, hobbies: "Despotism", country_of_birth: "Westeros", height: "167cm"},
   {name: "Norman Bates", cohort: :november, hobbies: "Hospitality", country_of_birth: "USA", height: "181cm"}
 ]
-=end
 
+=begin
 def input_students
   # create an empty array
   students = []
@@ -27,7 +27,7 @@ def input_students
       cohort = gets.chomp.downcase.to_sym
       #add the student hash to the array
       students << {name: name, cohort: cohort}
-      puts "Now we have #{students.count} students"
+      puts "Now we have #{students.count} #{students.count == 1 ? "student" : "students"}"
     else
       break
     end
@@ -35,6 +35,7 @@ def input_students
   #return the array of students
   students
 end
+=end
 
 def print_header
   puts "The students of Villains Academy".center(60)
@@ -42,20 +43,27 @@ def print_header
 end
 
 def print(students)
-  i = 0
-  l = students.length
-  while i < l
-    puts "#{i + 1}. #{students[i][:name]} (#{students[i][:cohort]} cohort)".center(60)
-    i += 1
+  
+  # iterate through students, create array of cohorts
+  cohorts = []
+  students.each do |student|
+    cohorts << student[:cohort]
   end
+
+  # iterate through that array, print each student in students where the cohort matches
+  cohorts.uniq.each do |cohort|
+    students.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)" if student[:cohort] == cohort }
+  end
+    
+  
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students".center(60)
+def print_footer(students)
+  puts "Overall, we have #{students.count} great #{students.count == 1 ? "student" : "students"}".center(60)
 end
 
 # nothing happens until we call the methods
-students = input_students
+# students = input_students
 print_header
 print(students)
 print_footer(students)
